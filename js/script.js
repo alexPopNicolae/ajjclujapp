2eea000858997af5f12d06ba82fdf00345a5547a
Alex Pop <alexpop3u@gmail.com>
2018-02-27 21:31:19 +0200

    latest updates to ajjclujapp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log("Totul merge cum trebuie");
+var element = document.getElementById('team');
+var computedStyles = window.getComputedStyle(element);
+console.log("Asta e ceea ce ma intereseaza:: ");
+console.log(computedStyles.getPropertyValue("border"));
+
